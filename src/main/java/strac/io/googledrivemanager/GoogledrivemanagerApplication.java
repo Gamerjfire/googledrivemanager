fa82a518d3f0a817fe2023b25bd5b51084e7725b
JudsonStangler <gamerjfire@gmail.com>
2024-10-02 13:59:35 -0500

    Base from SpringInitializer

---
+++ src/main/java/strac/io/googledrivemanager/GoogledrivemanagerApplication.java
@@ -0,0 +1,13 @@
+package strac.io.googledrivemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogledrivemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogledrivemanagerApplication.class, args);
+	}
+
+}
